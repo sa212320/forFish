dc5a6961c3ddb584908aafc715f7ea57698c45db
sa212320 <sa212320@gmail.com>
2017-11-12 15:09:06 +0800

    test2

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express(); //建立一個Express伺服器
 
-app.listen(8080, function () {
-  console.log('Example app is running on port 8080!');
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.listen(server_port, server_ip_address, function () {
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
 
 app.get('/', function (req, res) {
